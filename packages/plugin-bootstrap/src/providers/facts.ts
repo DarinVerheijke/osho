c6fcc37f108d3ae83b3d4106077d15efea0ddf0c
MetelskyVsevolod <metelskyvsevolod@gmail.com>
2024-11-25 23:23:05 +0900

    Debug stuff for relevant memories

--- packages/plugin-bootstrap/src/providers/facts.ts
+++ packages/plugin-bootstrap/src/providers/facts.ts
@@ -25,21 +25,33 @@ const factsProvider: Provider = {
             tableName: "facts",
         });
 
-        const relevantFacts = [];
-        //  await memoryManager.searchMemoriesByEmbedding(
-        //     embedding,
-        //     {
-        //         roomId: message.roomId,
-        //         count: 10,
-        //         agentId: runtime.agentId,
-        //     }
-        // );
+        let fetchRelevantFactsAmountString = runtime.getSetting("FETCH_RELEVANT_FACTS_AMOUNT")
+        let fetchRelevantFactsAmount = parseInt(fetchRelevantFactsAmountString) || 10;
+
+        const relevantFacts = await memoryManager.searchMemoriesByEmbedding
+        (
+             embedding,
+             {
+                 roomId: message.roomId,
+                 count: fetchRelevantFactsAmount,
+                 agentId: runtime.agentId,
+             }
+         );
+
+        console.log("fetchRelevantFactsAmount: ", fetchRelevantFactsAmount);
+        console.log(`\n\n==============RELEVANT FACTS START==============:\n${relevantFacts}\n==============RELEVANT FACTS END==============\n\n`);
+
+        let fetchRecentFactsAmountString = runtime.getSetting("FETCH_RECENT_FACTS_AMOUNT")
+        let fetchRecentFactsAmount = parseInt(fetchRecentFactsAmountString) || 10;
 
         const recentFactsData = await memoryManager.getMemories({
             roomId: message.roomId,
-            count: 10,
+            count: fetchRecentFactsAmount,
             agentId: runtime.agentId,
         });
+
+        console.log("fetchRecentFactsAmount: ", fetchRecentFactsAmount);
+        console.log(`\n\n==============RECENT FACTS START==============:\n${recentFactsData}\n==============RECENT FACTS END==============\n\n`);
 
         // join the two and deduplicate
         const allFacts = [...relevantFacts, ...recentFactsData].filter(
